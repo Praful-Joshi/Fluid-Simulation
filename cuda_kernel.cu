c162d8ca70f62d2b59e37d3fd9a8842047ef5fac
Praful-Joshi <87381105+Praful-Joshi@users.noreply.github.com>
2025-04-07 15:15:26 -0400

    Create cuda_kernel.cu

---
+++ cuda_kernel.cu
@@ -0,0 +1,38 @@
+#include <cuda_runtime.h>
+#include <cuda_gl_interop.h>
+#include <device_launch_parameters.h>
+#include <math.h>
+
+struct Particle {
+    float x, y;
+};
+
+static cudaGraphicsResource* cudaVBO = nullptr;
+
+__global__ void updateParticlesKernel(Particle* particles, int count, float time) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < count) {
+        float baseX = (i % 1000) / 500.0f - 1.0f; // spread x from -1 to +1
+        float baseY = (i / 1000) / 500.0f - 1.0f;
+        particles[i].x = baseX + 0.01f * sinf(time + i * 0.001f);
+        particles[i].y = baseY + 0.01f * cosf(time + i * 0.001f);
+    }
+}
+
+extern "C" void registerVBOWithCUDA(unsigned int vbo) {
+    cudaGraphicsGLRegisterBuffer(&cudaVBO, vbo, cudaGraphicsMapFlagsWriteDiscard);
+}
+
+extern "C" void updateParticlesCUDAInterop(int count, float time) {
+    Particle* devPtr;
+    size_t size;
+
+    cudaGraphicsMapResources(1, &cudaVBO, 0);
+    cudaGraphicsResourceGetMappedPointer((void**)&devPtr, &size, cudaVBO);
+
+    int threads = 256;
+    int blocks = (count + threads - 1) / threads;
+    updateParticlesKernel<<<blocks, threads>>>(devPtr, count, time);
+
+    cudaGraphicsUnmapResources(1, &cudaVBO, 0);
+}
